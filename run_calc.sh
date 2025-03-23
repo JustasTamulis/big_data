13c77757e3ef45f309eb6b647f32ff977a042bc9
juta1001 <juta1001@login3.mif>
2025-03-23 20:59:25 +0200

    adding hpc test

---
+++ run_calc.sh
@@ -0,0 +1,10 @@
+# !/bin/sh
+# SBATCH --account=juta1001 ## -> tavo allocation‘as
+# SBATCH --job-name=A_50
+# SBATCH --output=../Results/slurm-out-%j.out
+# SBATCH --error=../Results/slurm-out-%j.err
+# SBATCH --partition=cpu ##->cpu/gpu?????
+# SBATCH --ntasks=1
+# SBATCH --cpus-per-task=1
+# SBATCH --time=6-0
+python3 ~/test_hpc.py
